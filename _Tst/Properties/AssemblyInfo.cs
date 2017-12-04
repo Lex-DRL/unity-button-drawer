6dc76249d653d3f5fd23ac7b1b9d22c307b28a9c
DRL <drl.aivik@gmail.com>
2017-12-04 16:21:27 +0300

    rename _Tst project as should

--- _Tst/Properties/AssemblyInfo.cs
+++ _Tst/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_Src")]
+[assembly: AssemblyTitle("_Tst")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("_Src")]
+[assembly: AssemblyProduct("_Tst")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
